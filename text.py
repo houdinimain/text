73349cfcf963de88dfa134aa466bd43d3539ee48
houdinimain <houdgondal@icloud.com>
2024-12-04 19:10:06 +0000

    Add files via upload

---
+++ text.py
@@ -0,0 +1,30 @@
+import pgzrun  
+def draw():
+    screen.clear()
+    screen.draw.text("welcome to pg zero",(300,200),color = "red")
+
+pgzrun.go()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ text2.py
@@ -0,0 +1,7 @@
+import pgzrun
+
+def draw():
+    screen.clear()
+    screen.draw.text("welcome to pygame",(300,200),color="green")
+
+pgzrun.go()
